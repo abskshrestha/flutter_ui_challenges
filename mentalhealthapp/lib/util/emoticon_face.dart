f721cf015ec340eae104dd9f78a131a8976a6edd
Abhishek Shrestha <81076450+abskshrestha@users.noreply.github.com>
2022-05-10 20:33:28 +0545

    Update

--- mentalhealthapp/lib/util/emoticon_face.dart
+++ mentalhealthapp/lib/util/emoticon_face.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
 class EmoticonFace extends StatelessWidget {
-  const EmoticonFace({ Key? key }) : super(key: key);
+  final String emoticonFace; //are these constructers
+
+  const EmoticonFace({ // are thiese constructers
+     Key? key,
+     required this.emoticonFace 
+     }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
+    return Container(
                     decoration: BoxDecoration(
                         color: Colors.blue[600],
                         borderRadius: BorderRadius.circular(12)),
-                    padding: EdgeInsets.all(12),
+                    padding: EdgeInsets.all(15),
                     child: 
-                    Text('😍'),
+                    Text(
+                      emoticonFace
+                    ),
       
     );
   }
